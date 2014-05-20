f8bb355175861817e56936b27429ea9a63401ee2
Daniele Procida <daniele@vurt.org>
2014-05-20 23:20:37 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
         'django-chained-selectbox',
     ],
     dependency_links=[
-        'https://github.com/evildmp/django-widgetry/archive/improve-link-schema.zip#egg=django-widgetry-evildmp',
-        'https://github.com/evildmp/django-chained-selectbox/archive/fk-compatibility.zip#egg=django_chained_selectbox-evildmp',
+        'https://github.com/evildmp/django-widgetry/archive/improve-link-schema.zip#egg=django-widgetry',
+        'https://github.com/evildmp/django-chained-selectbox/archive/fk-compatibility.zip#egg=django-chained-selectbox',
     ],
 )
